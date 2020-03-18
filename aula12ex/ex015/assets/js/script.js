f17125fb223da64f34360675c4975d477da11d94
Celson Fernando <celsonpuc@hotmail.com>
2020-03-17 23:56:28 -0300

    Adicionando imagens e organizando pastas com assets

---
+++ aula12ex/ex015/assets/js/script.js
@@ -0,0 +1,65 @@
+function verificar() {
+    let dateNow = new Date();
+    let ano = dateNow.getFullYear();
+    let fano = document.getElementById('txtAno');
+    let resp = document.querySelector('#resp');
+
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert("[ERROR] Verifique o ano inserido e tente novamente! \nPor Favor digite uma data menor que o ano atual.");
+    }
+    else {
+        let sexVal = document.getElementsByName('radsex')
+        let idade = ano - Number(fano.value);
+        let genr = '';
+        let img = document.createElement('img');
+
+        img.setAttribute('class', 'foto');
+        img.setAttribute('class','moldura');
+
+        if (sexVal[0].checked) {
+            genr = "Homem";
+            if (idade >= 0 && idade < 10) {
+               img.setAttribute('src','assets/imgs/img-child-trat.png');
+              
+            }
+            else {
+                if (idade < 21) {
+                    img.setAttribute('src','assets/imgs/img-child-trat.png');
+                   
+                }
+                else {
+                    if (idade < 50) {
+                        img.setAttribute('src','assets/imgs/img-man-trat.png');
+                       
+                    } else {
+                        img.setAttribute('src','assets/imgs/img-man-old-trat.png');
+                        
+                    }
+                }
+            }
+        }
+        else {
+            if (sexVal[1].checked) {
+                genr = "Mulher";
+                if (idade >= 0 && idade < 10) {
+
+                }
+                else {
+                    if (idade < 21) {
+
+                    }
+                    else {
+                        if (idade < 50) {
+
+                        } else {
+
+                        }
+                    }
+                }
+            }
+        }
+        resp.style.textAlign = 'center';
+        resp.innerHTML = `<p>Nós detectamos ${genr} com a ${idade} anos.<p/>`;
+        resp.appendChild(img);
+    }
+}
