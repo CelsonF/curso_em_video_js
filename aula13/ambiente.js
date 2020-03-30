bbcee8e0785c1bb9b04b217bb82fd082120c058a
Celson Fernando <celsonpuc@hotmail.com>
2020-03-29 23:03:48 -0300

    Aprendendo o básico sobre estruturas de repetições

---
+++ aula13/ambiente.js
@@ -0,0 +1,28 @@
+//While com teste logico no final 
+/*let counts = 0;
+do {
+   console.log( ` ${counts+1}º Tudo bem ?`);
+   counts++;
+}while(counts < 6)*/
+
+
+
+ //while com teste logico no inicio
+ /*let counts = 0;
+ while(counts <= 3){
+    console.log( ` ${counts+1}º Tudo bem ?`);
+    counts = counts + 1;
+ }*/
+
+
+
+
+
+// console.log('helo ?');
+// console.log('helo ?');
+// console.log('helo ?');
+// console.log('helo ?');
+// console.log('helo ?');
+// console.log('helo ?');
+// console.log('helo ?');
+// console.log('helo ?');
---
+++ aula14/ambiente.js
@@ -0,0 +1,8 @@
+//for  - other structure repetition 
+
+//for( inicialization;  condition; increment )
+console.log("Testando o Debug");
+for (let c = 0; c < 10 ; c++){
+   console.log(` ${c+1}º Tudo bem?`);
+}
+console.log('End program');
